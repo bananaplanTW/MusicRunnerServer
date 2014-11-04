9b5a12be4378d007abf652270645bb9b95357f5f
Daz <daz.lee1987@gmail.com>
2014-11-04 21:04:18 +0800

    clean code

--- stores/TrackInfo.js
+++ stores/TrackInfo.js
@@ -1,58 +1,19 @@
 var moduleTrackInfoEchoNest = require('../modules/ModuleTrackInfoEchoNest').getInstance(),
     errorLog = require('../lib/logger').errorLog;
 
-moduleTrackInfoEchoNest.getTrackInfo("Maroon 5", "Maps", function (error, data) {
-                        if (error) {
-                                errorLog.error("[stores/TrackInfo.js][Error] cannot getting track info");
-                                errorLog.error(error);
-
-                                console.log("[stores/TrackInfo.js][Error] cannot getting track info");
-                                console.log(error);
-                                console.log(data);
-                        } else {
-                        }
-console.log(data);
-                });
-
 exports.getTrackInfo = function (songList, callback) {
 	var length = songList.length;
-	/*songList.forEach(function (song) {
-		moduleTrackInfoEchoNest.getTrackInfo(song.artist, song.title, function (error, data) {
-			length--;
-			if (error) {
-				errorLog.error("[stores/TrackInfo.js][Error] cannot getting track info");
-				errorLog.error(error);
-				song.bpm = -2;
-			} else {
-				song.bpm = data.bpm;
-			}
-
-			if (length == 0) {
-				callback(null, JSON.stringify(songList));
-				return
-			}
-		});
-	});*/
-
-console.log(songList);
 	function getTrackInfo (index) {
-console.log("get track");
-console.log(songList[index]);
 		var song = songList[index];
 		moduleTrackInfoEchoNest.getTrackInfo(song.artist, song.title, function (error, data) {
 			length--;
 			if (error) {
 				errorLog.error("[stores/TrackInfo.js][Error] cannot getting track info");
 				errorLog.error(error);
-
-				console.log("[stores/TrackInfo.js][Error] cannot getting track info");
-				console.log(error);
-				console.log(data);
 				song.bpm = -2;
 			} else {
 				song.bpm = data.bpm;
 			}
-console.log(song);
 			if (length == 0) {
 				callback(null, JSON.stringify(songList));
 				return
@@ -64,9 +25,6 @@ console.log(song);
 			setTimeout(function() {
 					getTrackInfo(index)
 				}, 100);
-		} else {
-			//callback(null, JSON.stringify(songList));
-			//return;
 		}
 	}
 	if (songList.length > 0) {
@@ -74,7 +32,4 @@ console.log(song);
 	} else {
 		callback(null, JSON.stringify(songList));
 	}
-	
-
-
 };
