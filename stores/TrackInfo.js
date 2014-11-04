60b3fc080bfc8703e078505eea951ecfbabbf0d5
Daz <daz.lee1987@gmail.com>
2014-11-04 16:56:32 +0800

    set the calling period of trackinfo to 100ms

--- stores/TrackInfo.js
+++ stores/TrackInfo.js
@@ -20,4 +20,39 @@ exports.getTrackInfo = function (songList, callback) {
 			}
 		});
 	});
+
+
+	function getTrackInfo (index) {
+		var song = songList[index];
+		moduleTrackInfoEchoNest.getTrackInfo(song.artist, song.title, function (error, data) {
+			length--;
+			if (error) {
+				errorLog.error("[stores/TrackInfo.js][Error] cannot getting track info");
+				errorLog.error(error);
+				song.bpm = -2;
+			} else {
+				song.bpm = data.bpm;
+			}
+
+			if (length == 0) {
+				callback(null, JSON.stringify(songList));
+				return
+			}
+		});
+
+		index ++;
+		if (index < songList.length) {
+			setTimeout(getTrackInfo(index), 100);
+		} else {
+			callback(null, JSON.stringify(songList));
+		}
+	}
+	if (songList.length > 0) {
+		getTrackInfo(0);
+	} else {
+		callback(null, JSON.stringify(songList));
+	}
+	
+
+
 };
