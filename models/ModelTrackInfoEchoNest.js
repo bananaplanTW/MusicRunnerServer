f5eca670a11feeba2abc9fc874634e1b9669a3c5
Daz <daz.lee1987@gmail.com>
2014-11-12 12:13:07 +0800

    save song info into database

--- models/ModelTrackInfoEchoNest.js
+++ models/ModelTrackInfoEchoNest.js
@@ -1,13 +1,45 @@
 var ModelBase = require("./ModelBase"),
     urls = require("../data/opendataUrls"),
     util = require('util'),
+    db   = require('connectDB'),
     errorLog = require('../lib/logger').errorLog,
     echoNestKey = process.env.ECHONESTKEY;
+var selectSongQuery = "SELECT * FROM music WHERE artist=%s AND title=%s";
+var insertSongQuery = "INSERT INTO music " + \
+					   "(artist, title, tempo, genre, duration, energy, liveness, speechiness, acousticness, instrumentalness, loudness, valence, danceability) VALUES " + \
+					   "('%s', '%s', %s, '%s', %s, %s, %s, %s, %s, %s, %s, %s, %s)";
 
 var ModelTrackInfoEchoNest = function () {
 
 };
 util.inherits(ModelTrackInfoEchoNest, ModelBase);
+
+var getSongFromDB = function (artist, title, callback) {
+	var quertString = util.format(selectSongQuery, artist, title);
+	db.execute(quertString, function (error, rows) {
+		if (error) {
+			callback(error, null);
+			return;
+		}
+		var row = rows[0];
+		console.log(rows);
+		callback(null, row)
+	});
+};
+
+var insertSongIntoDB = function (data, callback) {
+	var queryString = util.format(insertSongQuery, data.artist, data.title, data.tempo, data.genre, data.duration, data.energy, data.liveness, data.speechiness, data.acousticness, data.instrumentalness, data.loudness, data.valence, data.danceability);
+	db.execute(queryString, function(error, rows) {
+		if(error) {
+			console.log(error);
+			callback(error, null);
+			return;
+		}
+		console.log(rows);
+		callback(null, rows);
+		return;
+	});
+};
 
 ModelTrackInfoEchoNest.prototype.getTrackInfo = function(artist, title, callback) {
 	var echoNestUrl = util.format(urls.EchoNestTrackApi, echoNestKey, encodeURIComponent(artist), encodeURIComponent(title));
@@ -20,6 +52,11 @@ ModelTrackInfoEchoNest.prototype.getTrackInfo = function(artist, title, callback
             callback(error, null);
             return;
 		}
+		var songMetaData = JSON.parse(data).response.songs[0].audio_summary;
+		songMetaData.artist = artist;
+		songMetaData.title = title;
+		console.log(songMetaData);
+
 		callback(null, JSON.parse(data));
 		return;
 	});
